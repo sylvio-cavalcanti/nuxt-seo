cb3c5fa19027f9b49659f0f13137bc193034d05c
Sylvio Cavalcanti <sylviocavalcanti2020@gmail.com>
2023-11-28 09:20:52 -0300

    Debug

--- app.vue
+++ app.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
-    <p>Testing for auto deploy...</p>
-    <NuxtWelcome />
+    <p>SEO Debug</p>
   </div>
 </template>
+
+<script setup lang="ts">
+useSeoMeta({
+  title: 'My Amazing Site',
+  ogTitle: 'My Amazing Site',
+  description: 'This is my amazing site, let me tell you all about it.',
+  ogDescription: 'This is my amazing site, let me tell you all about it.',
+  ogImage: 'https://evenyx.s3.amazonaws.com/mailing/images/evenyx-branco.png',
+  twitterCard: 'summary_large_image',
+})
+</script>
